0a0cec06a70aa6b728d3784cb7c773a69d623ccf
maxmudovismoi <urgaz123777@gmail.com>
2022-09-29 09:14:50 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,72 @@
+//1 Masala???//
+// let b = 5
+
+// function generateAndSum(a){
+//     let arr = []
+//     let sum = 0
+//     for(let i=1; i<=a; i++){
+//         sum+=i
+//         arr.push(i)
+//     }
+//     return arr + "=>" + sum
+// }
+
+// console.log(generateAndSum(b));
+
+
+
+
+
+
+
+
+
+
+
+// 2chi massal//
+
+// function sumOfNegatives(event){
+//     let sum = 0
+//     for(let i of event){
+//         if(i<0){
+//             sum+=i
+//         }
+//     }
+
+//     return sum
+// }
+
+// console.log(sumOfNegatives([1,2,3,4,-5,-9, -12, 98]));
+
+
+
+
+
+// 3chi masala//
+
+// function sumOfNegatives(a, b){
+//     let sumA = 0
+//     let sumB = 0
+//     for(let i of a){
+//        sumA+=i
+//     }
+//     for(let i of b){
+//        sumB+=i
+//     }
+
+//     return sumA + sumB
+// }
+
+// console.log(sumOfNegatives([1,2,3,4], [1,2,3,4]));
+
+
+// 4chi masala //
+// function convert(a){
+//     if(a){
+//         return 'Yes'
+//     }else{
+//         return 'No'
+//     }
+// }
+
+// console.log(convert(true))
